15ce8ec392b3760ab6700e526f54ae9c24caa1e4
Walker-Walker <awalker0@gmail.com>
2020-10-01 14:30:44 -0700

    added ultils directory with helper.js file

---
+++ src/components/Nav/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function index() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+// import React, { Component } from 'react'  // class component 
+
+// export default class index extends Component {
+//     render() {
+//         return (
+//             <div>
+                
+//             </div>
+//         )
+//     }
+// }
