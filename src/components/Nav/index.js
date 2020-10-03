1f638892a5cd488d4c1af733ad0271648238a636
Walker-Walker <awalker0@gmail.com>
2020-10-02 21:20:14 -0700

    finished Nav Component, utilized capiitalize helper function, use state method, destructured pages for the use state in app.js then destructured use state and set equal to props in Nav component in order for app.js to have access to dom manipulation

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React from "react";
+import {capitalizeFirstLetter} from '../../utils/helpers';
 
-export default function index() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Nav = (props) => {
+  const { pages = [], currentPage, setCurrentPage } = props;
+  return (
+    <header className="flex-row">
+      <h1> Walker </h1>
 
-// import React, { Component } from 'react'  // class component 
+      <nav>
+        <ul className="flex-row">
+          {pages.map((page) => (
+            <li
+              className={`mx-1 ${
+                currentPage.name === page.name && `navActive`
+              }`}
+              key={page.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentPage(page);
+                }}
+              >
+                {capitalizeFirstLetter(page.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-// export default class index extends Component {
-//     render() {
-//         return (
-//             <div>
-                
-//             </div>
-//         )
-//     }
-// }
+export default Nav;
