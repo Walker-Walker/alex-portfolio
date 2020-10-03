ca61691f62771baff30eb27be560210cae15a096
Walker-Walker <awalker0@gmail.com>
2020-10-03 15:59:09 -0700

    destructured props into card component as title image  and tools , and returned inside div card wrapper

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Card.css"
-export default function Card() {
-  return <div className="card card-1"></div>;
+export default function Card({title, image, tools}) {
+  return (
+  <div className="card card-1">
+      {title} {image} {tools}
+  </div>
+
+  );
 }
