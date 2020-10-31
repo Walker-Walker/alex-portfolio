cef42827a46e87498b59cbce57c6376377b32570
Walker-Walker <awalker0@gmail.com>
2020-10-31 13:03:18 -0700

    added photo, trying to figure out why images aren't rendering?

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Card.css";
-export default function Card({ title, image, link, tools }) {
+export default function Card({title, tools, image, link }) {
   return (
 
     <div className="card-container">
