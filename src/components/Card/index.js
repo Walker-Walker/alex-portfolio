c19e356923e3a199da653537472373974285f95d
Walker-Walker <awalker0@gmail.com>
2021-01-31 21:18:54 -0800

    re-working the  card component jsx , trying to add more details for each card, perhaps utilize react bootstrap styling

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,23 +1,36 @@
 import React from "react";
 import "./Card.css";
 
-export default function Card({title, tools, image, link }) {
+export default function Card({ title, tools, image, link, Github }) {
   return (
-
     <div className="grid-container1">
-     
-    <a style={{display:"table-cell"}} href={link} target="_blank" rel="noopener noreferrer"> 
-      <div className="card card-1 card-grid">
-        <img className="img" src={image}/>
-        Title:{title} Tools:{tools} 
-      </div>
-    </a>
 
- {/* <Card.Body>
-   <section className="card-title">{title}</section>
-  <section className="card-tools">{tools}</section>
- </Card.Body> */}
+      
+
+      
+
+      <div className="card card-1">
+
+      <a // used to make the image a and all paramaters passed in one clickable link as well as open in new tab//
+      
+        style={{ display: "table-cell" }}
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        {title}
+        
+      </a>
+        <img className="img" src={image}/>
+        <br></br>
+        Technologies:{tools}
+        <br></br>
+        <a href={Github} className="git-style">
+          Github Repository
+        </a>
+      </div>
+      
+    
     </div>
   );
 }
-
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,54 +1,47 @@
-import React from 'react';
+import React from "react";
 // import { makeStyles } from '@material-ui/core/styles';
 // import Button from '@material-ui/core/Button';
-import "./portfolio.css"
+import "./portfolio.css";
 import Card from "../Card";
 
 export default function Portfolio() {
   const projects = [
-    
     {
       title: "Shelter Connect",
       image: "screenshot.jpg", //default image paths in public are recognized by React so no need to write path if image is in public
       tools: "NodeJs, Mysql2, Sequelize, Bootstrap ",
-      link:"https://shelter-connection.herokuapp.com/"
-      
+      Github: " https://github.com/Walker-Walker/Shelter_App",
+      link: "https://shelter-connection.herokuapp.com/",
     },
     {
       title: "Covid-19 Data",
       image: "screenshot2.png",
       tools: "NodeJs, Express Js, Heroku",
-      link:"https://covid-19-data-data.herokuapp.com/"
-      
+      Github:"https://github.com/Walker-Walker/Covid-19-Data-",
+      link: "https://covid-19-data-data.herokuapp.com/",
     },
     {
       title: "What's in your Fridge?",
       image: "fridgescaled.png",
       tools: "NodeJs, MERN, Bootstrap, GraphQL, Mongoose, MongoDB",
-      link:"https://whats-in-your-fridge-project3.herokuapp.com/"
-      
-    }
+      Github:"https://github.com/TheresaRutledge/whats-in-your-fridge",
+      link: "https://whats-in-your-fridge-project3.herokuapp.com/",
+    },
     // {
     //   title: "Shelter Connect",
     //   image: "path here",
     //   tools: "NodeJs",
     //   link:"https://shelter-connection.herokuapp.com/"
-      
+
     // },
   ];
   return (
-
-<div className="grid">
- {projects.map(project => {
-            return <Card {...project} key={project.title} />
-
-            
-            
-            
-  })}
-    
-          </div>
-  )
+    <div className="grid">
+      {projects.map((project) => {
+        return <Card {...project} key={project.title} />;
+      })}
+    </div>
+  );
 
   // jsx img src links href and css grid
   // return div cards  in responsive grid layout
