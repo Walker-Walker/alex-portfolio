51e8341328960e6add9140a469aba77f4808e59d
Walker-Walker <awalker0@gmail.com>
2021-02-01 19:56:02 -0800

    used inline styling to make links open into new tabs, tweaked some bootstrap styling for the cards, I found that since I am dynamically rendering each card from one function, I cant seem to make manipulate their placement, its just default side by side. I've tried grid , bootstrap , and flex box, I cannot seem to get the cards to wrap.

--- src/components/Projectcard/index.js
+++ src/components/Projectcard/index.js
@@ -1,49 +1,62 @@
 import React from "react";
 import "./Projectcard.css";
-import Card from 'react-bootstrap/Card'
+import Card from "react-bootstrap/Card";
+import { Container, Row, Col } from "react-bootstrap";
 
-export default function Projectcard({ title, tools, image, link, Github, description }) {
+export default function Projectcard({
+  title,
+  tools,
+  image,
+  link,
+  Github,
+  description,
+}) {
   return (
-    <div className="grid-container1">
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={image}/>
-  <Card.Body>
-    <Card.Title>{title}</Card.Title>
-    <Card.Text>
-      Tools used:{tools}
-    </Card.Text>
-    {description}
-  </Card.Body>
-  {/* <ListGroup className="list-group-flush">
-    <ListGroupItem>Cras justo odio</ListGroupItem>
-    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-    <ListGroupItem>Vestibulum at eros</ListGroupItem>
-  </ListGroup> */}
-  <Card.Body>
-    <Card.Link href={link}>{title}</Card.Link>
-    <Card.Link href={Github}>Github</Card.Link>
-  </Card.Body>
-</Card>
-      
+    <Container fluid>
+      <Row>
+        <Col>
+          <Card style={{ width: "20rem" }}>
+            <Card.Img variant="top" src={image} />
+            <Card.Body>
+              <Card.Title>{title}</Card.Title>
+              <Card.Text>Tools used:{tools}</Card.Text>
+              {description}
+              <Card.Link className="link-style" href={link} style={{display:"table-cell"}} target="_blank" rel="noopener noreferrer">
+                {title}
+                
+              </Card.Link>
+              <Card.Link className="link-style" href={Github} style={{display:"table-cell"}} target="_blank" rel="noopener noreferrer">
+                Github
+              </Card.Link>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row> 
+    </Container>
 
-      
-{/* 
-      <div className="card card-1">
+    // <div className="grid-container1">
 
-      
-        {title}
-        
-      </a>
-        <img className="img" src={image}/>
-        <br></br>
-        Technologies:{tools}
-        <br></br>
-        <a href={Github} className="git-style">
-          Github Repository
-        </a>
-      </div> */}
-      
-    
-    </div>
+    //   <div className="card card-1">
+
+    //   <a // used to make the image a and all paramaters passed in one clickable link as well as open in new tab//
+
+    //     style={{ display: "table-cell" }}
+    //     href={link}
+    //     target="_blank"
+    //     rel="noopener noreferrer"
+    //     >
+    //     {title}
+
+    //   </a>
+    //     <img className="img" src={image}/>
+    //     <br></br>
+    //     Technologies:{tools}
+    //     <br></br>
+    //     <a href={Github} className="git-style">
+    //       Github Repository
+    //     </a>
+    //   </div>
+
+    // </div>
   );
 }
