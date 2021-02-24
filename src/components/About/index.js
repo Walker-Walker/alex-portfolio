10654324dccb2054d74de95e4eef6160d36bb287
Walker-Walker <awalker0@gmail.com>
2021-02-23 21:21:06 -0800

    re-styled the div cards used flexbox to align the cards underneath their container.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import coverImage from "../../assets/cover/alt-cover-image.JPG";
 import "./about.css";
-import Card from 'react-bootstrap/Card'
+import Card from "react-bootstrap/Card";
 
 function About() {
   return (
@@ -16,72 +16,70 @@ function About() {
         />
       </section>
       <section className="intro">
-
-      <Card border="primary" style={{ width: '25rem' , height: '30rem'}}>
-    <Card.Header></Card.Header>
-    <Card.Body>
-      <Card.Title></Card.Title>
-      <Card.Text>
-        Full Stack Web Developer with experience in political science, maritime
-        logistics management, and the consistent ability to adapt new
-        challenges. Adept at problem solving, developed and debugged mobile
-        friendly applications. Recently learned and applied new and unfamiliar
-        technologies quickly within the mern stack e.g. graphql,
-        apollo-server-express & react context api. Enthusiastic team player that
-        has demonstrated a strong desire to implement new and established
-        technologies.
-        
-      </Card.Text>
-    </Card.Body>
-  </Card>
+        <Card border="primary" style={{ width: "25rem", height: "30rem" }}>
+          <Card.Header></Card.Header>
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Text>
+              Full Stack Web Developer with experience in political science,
+              maritime logistics management, and the consistent ability to adapt
+              new challenges. Adept at problem solving, developed and debugged
+              mobile friendly applications. Recently learned and applied new and
+              unfamiliar technologies quickly within the mern stack e.g.
+              graphql, apollo-server-express & react context api. Enthusiastic
+              team player that has demonstrated a strong desire to implement new
+              and established technologies.
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </section>
-      
-      
-  <section className="skills">
-        
-  <Card border="primary" style={{ width: '15rem' }} >
-    <Card.Header></Card.Header>
-    <Card.Body>
-      <Card.Title>Skills</Card.Title>
-      <Card.Text>
-        
 
-        {/* <h1>Skills</h1> */}
+      <section className="skills">
+        <h1>Skills</h1>
+        <Card border="primary" style={{ width: "12rem", height: "12rem" }}>
+          <Card.Header></Card.Header>
+          <Card.Body>
+            <Card.Title></Card.Title>
+            <Card.Text>
+              <section className="languages">
+                <h2>Languages</h2>
 
-        <section className="languages">
-          <h2>Languages</h2>
-
-          <ul>
-            <li>Javascript</li>
-            <li>JQuery</li>
-            <li>HTML5</li>
-            <li>CSS3</li>
-          </ul>
-        </section>
-
-        <section className="apps">
-          <h2>Applications</h2>
-          <ul>
-            <li>NodeJs</li>
-            <li>Mysql2</li>
-            <li>Sequelize</li>
-            <li>Sqlite3</li>
-            <li>Bootstrap</li>
-            <li>Handlebars</li>
-            <li>MongoDB</li>
-            <li>Mongoose</li>
-            <li>Gitbash</li>
-            <li>ExpressJS</li>
-            <li>React</li>
-            <li>MERN</li>
-            <li>GraphQL</li>
-          </ul>
-        </section>
-    
-      </Card.Text>
-    </Card.Body>
-  </Card>
-        
+                <ul>
+                  <li>Javascript</li>
+                  <li>JQuery</li>
+                  <li>HTML5</li>
+                  <li>CSS3</li>
+                </ul>
+              </section>
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card border="primary" style={{ width: "18rem", height: "26rem" }}>
+          <Card.Header></Card.Header>
+          <Card.Body>
+            <Card.Title>Applications</Card.Title>
+            <Card.Text>
+              <section className="apps">
+                {/* <h2>Applications</h2> */}
+                <ul>
+                  <li>NodeJs</li>
+                  <li>Mysql2</li>
+                  <li>Sequelize</li>
+                  <li>Sqlite3</li>
+                  <li>Bootstrap</li>
+                  <li>Handlebars</li>
+                  <li>MongoDB</li>
+                  <li>Mongoose</li>
+                  <li>Gitbash</li>
+                  <li>ExpressJS</li>
+                  <li>React</li>
+                  <li>MERN</li>
+                  <li>GraphQL</li>
+                </ul>
+              </section>
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </section>
     </div>
   );
